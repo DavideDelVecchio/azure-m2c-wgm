4655e6cd3429e575684bbb8f4fd11fda543f3ac9
Chris Joakim <christopher.joakim@gmail.com>
2021-05-25 13:40:26 -0400

    mongo uninstall/reinstall instructions in mongod_macos_repair.sh

--- reference_app/databases/mongod_macos_repair.sh
+++ reference_app/databases/mongod_macos_repair.sh
@@ -8,3 +8,20 @@ mongod --config /usr/local/etc/mongod.conf --repair
 
 echo 'displaying log ...'
 cat /usr/local/var/log/mongodb/mongo.log
+
+# Reinstall:
+# $ alias | grep mongod
+# $ alias mongod_start='mongod --config /usr/local/etc/mongod.conf &'
+#   path:   /usr/local/var/log/mongodb/mongo.log
+#   dbPath: /usr/local/var/mongodb
+# $ brew uninstall mongodb-community@4.0
+# $ brew cleanup
+# $ rm -rf /usr/local/var/log/mongodb/ 
+# $ rm -rf /usr/local/var/mongodb 
+#
+# $ brew tap mongodb/brew
+# $ brew install mongodb-community@4.4
+# ... exit shell, start a new one
+#
+# $ $ mongo --version
+# MongoDB shell version v4.4.5
